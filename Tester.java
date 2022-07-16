8494706dcf0883df7a5dfe9a70260ab8541c6f60
Dineshkumar Ashokan <dineshkumar.a@prodapt.com>
2022-07-16 18:12:26 +0530

    Tester

---
+++ Tester.java
@@ -0,0 +1,14 @@
+package com.prodapt.basics;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		SimpleInterest.p=1000;
+		SimpleInterest.r=5;
+		SimpleInterest.t=3;
+		SimpleInterest.SI();
+		
+
+	}
+
+}
