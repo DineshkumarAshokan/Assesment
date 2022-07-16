31005fb9d08f6a2822d2deed5d00739d63afb1e5
Dineshkumar Ashokan <dineshkumar.a@prodapt.com>
2022-07-16 18:11:17 +0530

    Code

---
+++ SimpleInterest.java
@@ -0,0 +1,25 @@
+package com.prodapt.basics;
+
+public class SimpleInterest {
+	
+		
+   static int p;
+   static  int r;
+   static int t;
+   static int Amount;
+   static int SI;
+   
+   static void SI()
+   {
+   SI=(p*r*t)/100;
+   System.out.println("Simple Interest="+SI);
+   Amount=SI+p;
+   System.out.println("Total Amount="+Amount);
+   
+   
+   
+     
+   
+	}
+
+}
