83093fb5dfe96a19bf8245717f4707166ca23647
Dineshkumar Ashokan <dineshkumar.a@prodapt.com>
2022-07-16 18:13:18 +0530

    Code

---
+++ SchoolRecords.java
@@ -0,0 +1,19 @@
+package com.prodapt.basics;
+
+public class SchoolRecords {
+
+	
+		String name;
+		int id;
+		String address;
+		void dRecords(String school_name,int school_id,String School_address) {
+			System.out.println(school_name+" "+school_id+" "+School_address);
+		}
+		void dispRecords() {
+			System.out.println("This is the Record of the Schools");
+		}
+		
+		
+	}
+
+
