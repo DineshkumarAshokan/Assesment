ed5a2dbe7984d22174a12446330124ecb6a3ac69
Dineshkumar Ashokan <dineshkumar.a@prodapt.com>
2022-07-16 18:13:50 +0530

    Tester

---
+++ Tester2.java
@@ -0,0 +1,14 @@
+package com.prodapt.basics;
+
+public class Tester2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SchoolRecords sc=new SchoolRecords();
+		sc.dRecords("ABC School",2001,"No 8J3JV+5H9, SR. Wing, B2 Janakpuri, New Delhi, Delhi 110058");
+		sc.dRecords("XYZ School",2034," A-4/C Block, Janakpuri, New Delhi, Delhi 110058");
+		sc.dRecords("DEF School",2653," J839+65Q, Shaheed Budh Ram Singh Marg, Lahore Apartment, Vasundhara Enclave, New Delhi, Delhi 110096");
+		sc.dispRecords();
+	}
+
+}
