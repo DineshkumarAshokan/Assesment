a33aa3f931f72c2175bbe3e0169ca9ac5eceef86
DineshkumarAshokan <108718250+DineshkumarAshokan@users.noreply.github.com>
2022-07-18 16:54:19 +0530

    Update Tester2.java

--- Tester2.java
+++ Tester2.java
@@ -5,9 +5,10 @@ public class Tester2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		SchoolRecords sc=new SchoolRecords();
-		sc.dRecords("ABC School",2001,"No 8J3JV+5H9, SR. Wing, B2 Janakpuri, New Delhi, Delhi 110058");
-		sc.dRecords("XYZ School",2034," A-4/C Block, Janakpuri, New Delhi, Delhi 110058");
-		sc.dRecords("DEF School",2653," J839+65Q, Shaheed Budh Ram Singh Marg, Lahore Apartment, Vasundhara Enclave, New Delhi, Delhi 110096");
+		sc.dRecords("Don Bosco Mat. Hr. Sec. School",2001,"13, CASA Major Rd, Egmore, Chennai, Tamil Nadu 600008
+A 4-min walk from the Government Museum, Chennai");
+		sc.dRecords("Chennai Boys Higher Secondary School",2034,"  27, Rajaji Street, Division- 107, Corporation School Road, Lake Area, Nungambakkam, Chennai, Tamil Nadu 600034");
+		sc.dRecords("St Francis International School",2653," 79, Anna Main Rd, near Porur, Jayapriya Nagar, Kolapakkam, Chennai, Tamil Nadu 600128");
 		sc.dispRecords();
 	}
 
